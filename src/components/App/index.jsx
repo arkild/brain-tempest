9115863e807331d5230021ee3efd62cc5537b3b9
arkild <141771685+arkild@users.noreply.github.com>
2023-11-06 11:47:43 -0700

    starting to scaffold the app

---
+++ src/components/App/index.jsx
@@ -0,0 +1,6 @@
+import './styles.css'
+
+export default function App() {
+  return <h1>BrainTempest</h1>
+  //The getData function that's going to be used will not be pulling the API - instead, it's going to be pulling the data from the DB.
+}
