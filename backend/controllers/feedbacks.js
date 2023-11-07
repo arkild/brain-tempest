465360e4d86d4bc539599f213424d014f86c142d
arkild <141771685+arkild@users.noreply.github.com>
2023-11-07 13:09:54 -0700

    routes working for feedback create, update, delete

--- backend/controllers/feedbacks.js
+++ backend/controllers/feedbacks.js
@@ -16,7 +16,30 @@ router.post('/create/:ideaId', (req, res) => {
         req.params.ideaId,
         {$push: {feedback: req.body}},
         {new: true})
-        .then(idea => res.json(req.params.ideaId))
+        .then(idea => res.json(idea))
+        //This pulled up the idea itself and then the feedback about it. So now I need to do something similar with the update route.
+})
+
+//Reading - I haven't made the route yet; I don't know if it will actually be required as I've got full CRUD on the ideas themselves and the "reading" is done from the idea page's feedback section.
+
+// Update Route (This is referencing the feedback itself)
+router.put('/:id', (req, res) => {
+    db.Idea.findOneAndUpdate(
+        {'feedback._id': req.params.id},
+        {feedback: req.body},
+        {new: true})
+        .then(idea => res.json(idea))
+        //This pulled up the idea with the feedback array attached to it.
+})
+
+// Delete Route
+router.delete('/:id', (req, res) => {
+    //We are "deleting" the index of the array that pertains to the feedback left by using the "pull" method assigned to the specific feedback ID that was given.
+    db.Idea.findOneAndUpdate(
+        {'feedback._id': req.params.id},
+        {$pull: {feedback: {_id: req.params.id}}},
+        {new: true})
+        .then(idea => res.json(`deleted ${req.params.id}`))
 })
 
 //This line is needed or your middleware will break
