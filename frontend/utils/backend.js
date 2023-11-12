5557912ffeb8359d4948361fd3383b331b2eb03e
arkild <141771685+arkild@users.noreply.github.com>
2023-11-12 15:28:39 -0700

    adjusted backend file and config file properly

--- frontend/utils/backend.js
+++ frontend/utils/backend.js
@@ -6,54 +6,54 @@ const authHeader = {headers: {'Authorization': localStorage.getItem('userToken')
 // IDEA ROUTES
 //This pulls up specific details about an idea when you select it. This is also going to render the feedback that's available for the idea as it's attached to it via an Array field in the schema.
 export async function getIdea(ideaId) {
-    const {data} = await axios.get(`/ideas/${ideaId}`)
+    const {data} = await axios.get(`/db/ideas/${ideaId}`)
     return data
 }
 
 //Adding an idea to the database (requires auth)
 export async function postIdea(idea) {
-    const {data} = await axios.post('/ideas/', idea, authHeader)
+    const {data} = await axios.post('/db/ideas', idea, authHeader)
     return data
 }
 
 //Updating an idea in the database already (requires auth)
 export async function updateIdea(idea, id) {
-    const {data} = await axios.put(`ideas/${id}`, idea, authHeader)
+    const {data} = await axios.put(`/db/ideas/${id}`, idea, authHeader)
     return data
 }
 
 //Deleting an idea in the database (requires auth)
 export async function deleteIdea(id) {
-    const {data} = await axios.delete(`/ideas/${id}`, authHeader)
+    const {data} = await axios.delete(`/db/ideas/${id}`, authHeader)
     return data
 }
 
 // FEEDBACK ROUTES
 //Unsure about this one but I'm pretty sure I got it
 export async function postFeedback(id, feedback) {
-    const {data} = await axios.post(`/feedback/create/${id}`, feedback, authHeader)
+    const {data} = await axios.post(`/db/feedback/create/${id}`, feedback, authHeader)
     return data
 }
 //Updating a feedback comment (requires auth)
 export async function updateFeedback(id, feedback) {
-    const {data} = await axios.put(`/feedback/${id}`, feedback, authHeader)
+    const {data} = await axios.put(`/db/feedback/${id}`, feedback, authHeader)
     return data
 }
 //Delete a feedback comment (requires auth)
 export async function deleteFeedback(id) {
-    const {data} = await axios.delete(`/feedback/${id}`, authHeader)
+    const {data} = await axios.delete(`/db/feedback/${id}`, authHeader)
     return data
 }
 
 //USER ROUTES
 //Sign up
 export async function signUp(user) {
-    const {data} = await axios.post('/users/signup', user)
+    const {data} = await axios.post('/db/users/signup', user)
     return data
 }
 //Log in
 export async function logIn(user) {
     console.log("the login was attempted to be triggered")
-    const {data} = await axios.post('/users/login', user)
+    const {data} = await axios.post('/db/users/login', user)
     return data
 }
--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,15 @@ import path from 'path'
 export default defineConfig({
   root: path.resolve(__dirname, 'frontend'),
   plugins: [react()],
+  server: {
+    // Proxy is the connection between the front and back-end. My axios routes draw a reference to '/db', and once they do that, it pulls the "base" url from this target.
+    proxy: {
+      '/db': {
+        target: `http://localhost:${process.env.PORT}`,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  }
 })
