8f875d92241a64d4321694068c3fdf0b6e0acb67
arkild <141771685+arkild@users.noreply.github.com>
2023-11-13 03:11:00 -0700

    New Idea Form formatted

--- frontend/src/components/IdeaPage/index.jsx
+++ frontend/src/components/IdeaPage/index.jsx
@@ -48,52 +48,57 @@ export default function IdeaPage() {
 
     return (
         <div>
-            <div>
-                <h2>Create a new idea!</h2>
-                <form className="" onSubmit={handleSubmit}>
-                    <div>
+            <div className="text-center">
+                <h2 className="text-4xl mt-10 p-4 text-center">Create a new idea!</h2>
+                <form className="text-xl p-4 text-center" onSubmit={handleSubmit}>
+                    <div className="w-[60vw] mx-auto">
+                        <p className="">The name of your project:</p>
                         <input
                             name="name"
-                            className=""
-                            placeholder="The name of your project"
+                            className="bg-gray-700 w-[60vw] rounded-lg text-center resize-none"
+                            placeholder="Facekickers Anonymous"
                             value={createFormData.name}
                             onChange={handleInputChange}
                         />
                         <br/>
+                        <p className="">What's the problem your project is trying to solve?</p>
                         <textarea
                             name="problem"
-                            className=""
-                            placeholder="What is the problem your project is trying to solve?"
+                            className="bg-gray-700 w-[60vw] rounded-lg text-center resize-none"
+                            placeholder="I want to be able to kick people in the face, but I don't want to get in trouble for doing it"
                             value={createFormData.problem}
                             onChange={handleInputChange}
                         />
                         <br/>
+                        <p className="">To add an image, please paste the link to it <i>(upload functionality coming soon!)</i></p>
                         <input
                             name="image"
-                            className=""
-                            placeholder="Paste the link to an image for your project here."
+                            className="bg-gray-700 w-[60vw] rounded-lg text-center resize-none"
+                            placeholder="(use '/phil.png' for testing if you don't have an image)"
                             value={createFormData.image}
                             onChange={handleInputChange}
                         />
                         <br/>
+                        <p className="">What are the features your idea currently has, or that you're planning to implement?</p>
                         <textarea
                             name="features"
-                            className=""
-                            placeholder="What are the features of your application?"
+                            className="bg-gray-700 w-[60vw] rounded-lg text-center resize-none"
+                            placeholder="I'll surprise you :D"
                             value={createFormData.features}
                             onChange={handleInputChange}
                         />
                         <br/>
+                        <p className="">Where would you like your feedback to be centered around? Where do you feel you need help?</p>
                         <textarea
                             name="needHelp"
-                            className=""
-                            placeholder="What kind of help are you looking for with your idea?"
+                            className="bg-gray-700 w-[60vw] rounded-lg text-center resize-none"
+                            placeholder="It's too hard to actually do what I want without getting arrested for assault"
                             value={createFormData.needHelp}
                             onChange={handleInputChange}
                         />
                         <div>
                             <button
-                                type="submit" className="">
+                                type="submit" className="submit bg-blue-800 text-white py-2 px-4 rounded-lg hover:bg-blue-900 m-2">
                                     Submit
                             </button>
                         </div>
