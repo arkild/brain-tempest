5d89d1d28c7621fa39dde31bff4e84657a2c46c1
arkild <141771685+arkild@users.noreply.github.com>
2023-11-13 14:30:39 -0700

    forgot to have landing page direct to the listings

--- frontend/src/components/LandingPage/index.jsx
+++ frontend/src/components/LandingPage/index.jsx
@@ -12,7 +12,7 @@ export default function LandingPage() {
         <p className="border-orange-500 border-b">Now I haven't gotten a lawyer on call to discuss the legality and copyright protection of the people who are working on the ideas that they've posted on this website, so we're just going to go on an honor system and have you agree that you won't be mean and steal any of the ideas within this website by having you click on the button down here to continue. <i>(You can click the links on the navbar above to bypass this button situation, but then I'd silently judge you for doing it.)</i></p>
         </div>
         <div>
-        <Link to="/"><button class="bg-purple-800 text-white py-2 px-4 rounded-lg hover:bg-purple-900 mt-4">
+        <Link to="/home"><button class="bg-purple-800 text-white py-2 px-4 rounded-lg hover:bg-purple-900 mt-4">
             Sounds good; let's go
         </button></Link>
         </div>
