fb9b1fbdafb21d6b2c5882974bac23513b5b1c03
arkild <141771685+arkild@users.noreply.github.com>
2023-11-13 00:42:05 -0700

    delete functionality works for ideas

--- frontend/src/components/DetailsPage/index.jsx
+++ frontend/src/components/DetailsPage/index.jsx
@@ -1,9 +1,24 @@
 import FeedbackSection from "../FeedbackSection"
+//This will help to redirect the delete to the main page.
+import {useNavigate} from "react-router-dom"
+//Import the specific functionality to delete the idea
+import {deleteIdea} from "../../../utils/backend"
 
 //Depending on where I get this data, sometimes I'll have to deconstruct it using {}, and sometimes I don't. It's weird but if it works, it works.
 export default function DetailsPage(idea) {
+    //I'm going to put on the ability to delete the idea directly on the idea's details page.
+    const navigate = useNavigate();
+
+    function handleDelete() {
+        console.log(idea._id)
+        deleteIdea(idea._id)
+        navigate("/home")
+        window.location.reload();
+    }
     return (
+        
         <>
+        {console.log(idea)}
         <div>
             <h1>{idea.name}</h1>
             <img src={idea.image} />
@@ -11,6 +26,7 @@ export default function DetailsPage(idea) {
             <p>Features: {idea.features}</p>
             <p>What do you need help with? {idea.needHelp}</p>
         </div>
+        <button onClick={handleDelete} className="">DELETE IDEA</button>
         <FeedbackSection ideaId={idea._id} />
         </>
     )
